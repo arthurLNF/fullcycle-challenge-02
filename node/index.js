4904ce268cf273010369fec467ef36045d99cb64
Arthur Nogueira <arthur.nogueira.ec@gmail.com>
2021-08-02 19:26:49 -0400

    Falta persistir os dados do schema

--- node/index.js
+++ node/index.js
@@ -1,8 +1,46 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+const mysql = require('mysql')
+
+var connection = mysql.createConnection(config)
+let sql = `INSERT INTO people(name) VALUES('João')`
+connection.query(sql)
+sql = `INSERT INTO people(name) VALUES('Pedro')`
+connection.query(sql)
+sql = `INSERT INTO people(name) VALUES('Paulo')`
+connection.query(sql)
+sql = `INSERT INTO people(name) VALUES('Lucas')`
+connection.query(sql)
+
+connection.end()
+
+function fetchPeople(callback) {
+    var connection = mysql.createConnection(config)
+    connection.query('SELECT * FROM people', function(err, rows) {
+        if (err) {
+            callback(err, null);
+        } else 
+            callback(null, rows);
+    });
+}
+
 app.get('/', (req, res) => {
-    res.send("<h1>Hello, Express!!!</h1>")
+    fetchPeople(function(err, data){        
+        let response = "<h1>Full Cycle Rocks!!!</h1>"
+        response += "<ul>"
+        data.forEach(person => {
+            response += `<li>${person.name}</li>`
+        });
+        response += "</ul>"
+        res.send(response)
+    })
 })
 
 app.listen(port, () => {
